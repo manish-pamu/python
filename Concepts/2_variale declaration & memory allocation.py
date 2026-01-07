10101f76617dd255f6023b84cae9bdaa4c5fc134
Manish Mani <manishpamu@hotmail.com>
2026-01-07 21:25:20 +0530

    Create 2_variale declaration & memory allocation.py

---
+++ Concepts/2_variale declaration & memory allocation.py
@@ -0,0 +1,17 @@
+#
+x = 10
+name_1 = 'mark'
+
+
+# get the address/memory_location of the value that is stored in a variable
+
+#
+x = 5
+print(id(x))
+    #output: 140713684923432
+
+#
+name = "Lollipop"
+print(id(name))
+    #output: 2007662932016
+
